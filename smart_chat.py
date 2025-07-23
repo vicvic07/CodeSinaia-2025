9b234733eb8a62977bca32bce44ab5b11c7d9673
vicvic07 <victor.c.griga@gmail.com>
2025-07-23 16:16:43 +0300

    Update smart_chat.py

--- smart_chat.py
+++ smart_chat.py
@@ -1,23 +1,3 @@
-#Refuz sa vad aceasta ANOMALIE este absolut INACCEPTABIL MIMIMIMIMMMIMIMIMIMIMIMIMIMIMI
-#MIMIMIMI si tie scumpica ❤❤❤H
-#Nu scapi de papat de data aceasta o sa vin dupa tine!! (Apropo mai vrei cornulete? :) ) o sa te pap eu pe tine prima data oricum deci tu nu scapi de asta deci skill issue
-#Uiiii acuma pot sa tastez in codul tau cat timp imi fixezi lama pe laptop (Haha, prinzi gluma? :))))) )
-#I mean eu cred ca s-a suparat ca i s-a spus "Blud" si s-a simtit ofensat de schizofrenia noastra ce a fost luata drept dorinta sinucigasa R: i-a fost spus sarmaluta totusi...
-#Cri-cri
-#hmmmmmmm ce pot sa mai scriu?
-#RETETA DE GATIT LAME PENTRU CA NU NE PLACE PITON
-#SE IA LAPTOPUL
-#SE INCINGE GRATARUL IN SUCEAVA (acolo langa copacul parjolit)
-#SE PUNE PE FOC LAPTOPUL LA 200(semn de grade) Celsius
-#SE ADAUGA MICII
-#SE PRAJESTE CARNEA + LITIUL 
-#Se iau lucrurile de pe foc si se mananca cu KETCHUP ;)))))))))) NUUUUUUU, O SA TE PAP!!! ATUNCI VEI PAPA SI TU KETCHUP!!!!
-#HAHAHA OENTR CA EU AM MANCAT MICI CU KETCHUP DECI DAC AMA PAPI MANANCI SI TU MICI CU KETCHUP SSSSSSS.S...... :))))))))))
-#o SA TE PAP CU MUSTAR DUPA FOARTEFOARTE TARE CA SA SE ANULEZE KETCHUPUL DIN TINE ATUNCI!!! NU O SA SUPRAVIETUIASCA NASUCUL ALA MITITEL ACUM :)))
-#POFTA BUNA PISICEL!!!! ❤❤❤❤❤❤❤❤❤❤❤❤❤❤ te pupicesc :))
-#O SA TREC LA RAND NOU PENTRU CA DA
-#NU VOI RECUNOASTE NICIODATA SUPRMATIA MUSTARULUI (bleah) CI VOI CONTINUA SA DOVEDESC CU FIECARE RASUFLARE A MEA ABILITATILE SUPERIOARE ALE KETCHUPULUI!! ALL HAIL THE QUEEN!!! 
-#ASTA PRESUPUNE CA O SA MAI FII DUPA CE TE PAP!!! (NU O SA MAI FII)
 
 from HelloOllama import SmartAgent as blud
 sarmaluta=blud ()
